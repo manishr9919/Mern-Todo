4859a25b3f65d12532305aafb8ebf043e9872e36
manishr9919 <manishr9919@gamil.com>
2025-10-16 12:00:45 +0530

    add-gitingnore

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://mern-todo-9crr.onrender.com/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
